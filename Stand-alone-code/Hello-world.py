08324942362cc927950c8d3db86409fa9ae0bb9e
Ryan Azmoodeh <ryan@Ryans-MacBook-Pro.local>
2020-04-14 16:16:05 -0700

    incremental commit

--- Stand-alone-code/Hello-world.py
+++ Stand-alone-code/Hello-world.py
@@ -8,9 +8,9 @@ import requests
 # print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = 'Hello, {}'.format(who_to_greet)
-#     return greeting
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
 
 
 r = requests.get('https://www.google.com')
