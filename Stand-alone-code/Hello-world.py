f1ad95c5b478fbd48e337b3d4a1353823e1de032
Ryan Azmoodeh <ryan@Ryans-MacBook-Pro.local>
2020-04-14 11:52:13 -0700

    Commented out some code

--- Stand-alone-code/Hello-world.py
+++ Stand-alone-code/Hello-world.py
@@ -4,13 +4,13 @@ from os import rename
 
 import requests
 
-# print(sys.version)
-print(sys.executable)
+# # print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
 
 
 r = requests.get('https://www.google.com')
